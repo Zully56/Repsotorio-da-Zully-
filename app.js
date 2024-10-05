c2d074a9adaccf79bec7c995f95c41b6ec380398
Zully  Dayana Vargas Escobar <160067100+Zully56@users.noreply.github.com>
2024-10-05 11:48:15 -0300

    Modulo externo

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-const readline = require('readline')
-
-//criando interface para ler entrad e saida do terminal 
-const rl= readline.createInterface({
-    input:process.stdin, //captura entrada
-    output:process.stdout //exibe saida do terminal 
-})
-
-//Faz uma pergunta ao usuario 
-rl.question(" Qual é seu nome " , (nome)=>{
-    console.log(`Ola ${nome}!`)
-    rl.close()
-})
+const color = require('colors')
+console.log("Hello World".bgBlue)
